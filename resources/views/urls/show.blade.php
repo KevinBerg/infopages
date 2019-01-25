5764c24841c688d5bc7894fa8339a34bf4594777
kedobe <kevin.berg@yahoo.de>
2019-01-25 13:18:29 +0100

    imrpoved url overview

--- resources/views/urls/show.blade.php
+++ resources/views/urls/show.blade.php
@@ -11,16 +11,24 @@
                         <thead>
                             <tr>
                                 <th>{{ __('Page Title') }}</th>
+                                <th>{{ __('Page Description') }}</th>
                                 <th>{{ __('URL') }}</th>
-                                <th>{{ __('Action') }}</th>
+                                <th>{{ __('Contents') }}</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($pages as $page)
                                 <tr>
-                                    <td>{{ $page->title }}</td>
+                                    <td><a href="{{ url('pages', [$page->id] ) }}">{{ $page->title }}</a></td>
+                                    <td>{{ $page->description }}</td>
                                     <td><a href="{{ url('render', [$page->title] ) }}" target="_blank">{{ url('render', [$page->title] ) }}</a></td>
-                                    <td><a href="{{ url('pages', [$page->id] ) }}">{{ __('Edit') }}</a></td>
+                                    <td>
+                                    @if ($page->contents)
+                                        @foreach ($page->contents as $content)
+                                            <p><a href="{{ url('contents', [$content->id] ) }}">{{ $content->title }} ({{ $content->status == true ? __('Active') : __('Inactive') }})</a></p>
+                                        @endforeach
+                                    @endif
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
