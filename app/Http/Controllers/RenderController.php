a4d3792ee2c6f61c8dff7e893d478638f0532096
Kevin Berg <mail@kevinberg.de>
2019-02-20 10:59:04 +0100

    getFilteredContents function

--- app/Http/Controllers/RenderController.php
+++ app/Http/Controllers/RenderController.php
@@ -29,48 +29,7 @@ class RenderController extends Controller
 
                 # retrieve filtered contents from cache
                 $contents = Cache::rememberForever($page->getContentsCacheIndex(), function () {
-
-                    $page = $this->currentPage;
-
-                    # get the page contents from the content_pages table.
-                    $contents = $page->contents;
-
-                    if($contents) {
-
-                        # filter active contents.
-                        $contents = $contents->where('status', 1);
-
-                        # find current highest priority
-                        $currentHighestPrio = 3;
-                        foreach($contents as $key => $content) {
-                            if($content->priority < $currentHighestPrio) {
-                                $currentHighestPrio = $content->priority;
-                                # 1 is the highes priority. Break if exists one content with highes prio.
-                                if($currentHighestPrio === 1) {
-                                    break;
-                                }
-                            }
-                        }
-
-                        # filter by highest priority
-                        foreach( $contents as $key => $content) {
-                            if($content->priority < $currentHighestPrio) {
-                                $contents->forget($key);
-                            }
-                        }
-
-                        # filter inactives by runtime
-                        foreach($contents as $key => $content) {
-                            $compareDate = $content->created_at->addDays($content->runtime);
-                            if(Carbon::now()->gt($compareDate)){
-                                $contents->forget($key);
-                            }
-                        }
-
-                        return $contents;
-
-                    }
-
+                    return $this->currentPage->getFilteredContents();
                 });
 
                 if(is_object($contents) && $contents->count()) {
--- app/Page.php
+++ app/Page.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Traits\HasRoles;
+use Carbon\Carbon;
 
 class Page extends Model
 {
@@ -27,6 +28,49 @@ class Page extends Model
     public function getContentsCacheIndex()
     {
         return $this->title.'_cached_contents';
+    }
+
+    public function getFilteredContents()
+    {
+
+        # get the page contents from the content_pages table.
+        $contents = $this->contents;
+
+        if($contents) {
+
+            # filter active contents.
+            $contents = $contents->where('status', 1);
+
+            # find current highest priority
+            $currentHighestPrio = 3;
+            foreach($contents as $key => $content) {
+                if($content->priority < $currentHighestPrio) {
+                    $currentHighestPrio = $content->priority;
+                    # 1 is the highes priority. Break if exists one content with highes prio.
+                    if($currentHighestPrio === 1) {
+                        break;
+                    }
+                }
+            }
+
+            # filter by highest priority
+            foreach( $contents as $key => $content) {
+                if($content->priority < $currentHighestPrio) {
+                    $contents->forget($key);
+                }
+            }
+
+            # filter inactives by runtime
+            foreach($contents as $key => $content) {
+                $compareDate = $content->created_at->addDays($content->runtime);
+                if(Carbon::now()->gt($compareDate)){
+                    $contents->forget($key);
+                }
+            }
+
+            return $contents;
+
+        }
     }
 
 }
